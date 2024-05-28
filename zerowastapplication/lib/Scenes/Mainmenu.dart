9431f30ec4711a74470eadcab105f359d4c4f69a
NonthawatKRT <nonnon48265566@gmail.com>
2024-05-29 00:29:46 +0700

    version1.0
    Add Custom Splashscreen , Decorate shimmer loading page

--- zerowastapplication/lib/Scenes/Mainmenu.dart
+++ zerowastapplication/lib/Scenes/Mainmenu.dart
@@ -30,12 +30,12 @@ class _MenuScreenState extends State<MenuScreen> {
   Widget build(BuildContext context) {
     return CustomScaffold(
       child: _isLoading
-          ? _buildShimmer() // Show shimmer if loading
+          ? _buildShimmer(_currentIndex) // Show shimmer if loading
           : _pages[_currentIndex],
       bottomNavigationBar: Container(
         color: const Color(0xFFC7E8D5),
         child: BottomNavigationBar(
-          backgroundColor: Color(0xFFC7E8D5),
+          backgroundColor: const Color(0xFFC7E8D5),
           currentIndex: _currentIndex,
           selectedItemColor: const Color.fromARGB(255, 0, 0, 0),
           unselectedItemColor: Colors.black54,
@@ -56,35 +56,35 @@ class _MenuScreenState extends State<MenuScreen> {
               icon: const Icon(Icons.home),
               label: 'หน้าแรก',
               backgroundColor: _currentIndex == 0
-                  ? Color(0xFFC7E8D5)
+                  ? const Color(0xFFC7E8D5)
                   : Colors.transparent,
             ),
             BottomNavigationBarItem(
               icon: const Icon(Icons.menu_book),
               label: 'เลือกเมนู',
               backgroundColor: _currentIndex == 1
-                  ? Color(0xFFC7E8D5)
+                  ? const Color(0xFFC7E8D5)
                   : Colors.transparent,
             ),
             BottomNavigationBarItem(
               icon: const Icon(Icons.add_shopping_cart),
               label: 'เพิ่มวัตถุดิบ',
               backgroundColor: _currentIndex == 2
-                  ? Color(0xFFC7E8D5)
+                  ? const Color(0xFFC7E8D5)
                   : Colors.transparent,
             ),
             BottomNavigationBarItem(
               icon: const Icon(Icons.add),
               label: 'เพิ่มเมนู',
               backgroundColor: _currentIndex == 3
-                  ? Color(0xFFC7E8D5)
+                  ? const Color(0xFFC7E8D5)
                   : Colors.transparent,
             ),
             BottomNavigationBarItem(
               icon: const Icon(Icons.inventory),
               label: 'คลัง',
               backgroundColor: _currentIndex == 4
-                  ? Color(0xFFC7E8D5)
+                  ? const Color(0xFFC7E8D5)
                   : Colors.transparent,
             ),
           ],
@@ -93,13 +93,324 @@ class _MenuScreenState extends State<MenuScreen> {
     );
   }
 
-  Widget _buildShimmer() {
-    return Shimmer.fromColors(
-      baseColor: Colors.grey[300]!,
-      highlightColor: Colors.grey[100]!,
-      child: Container(
-        color: Colors.grey[300],
-      ),
-    );
+  Widget _buildShimmer(int index) {
+    switch (index) {
+      case 0:
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+            color: Colors.grey[300],
+            child: Center(child: Text('Loading Home Page...')),
+          ),
+        );
+      case 1:
+  return Container(
+    color: Colors.grey[200], // Grey background
+    padding: const EdgeInsets.only(top: 0), // Add padding to start below the app bar
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+            height: 56.0, // Height of the app bar
+            color: Colors.grey[300],
+            child: Center(child: Text('Loading App Bar...')),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+              height: 50, // 50% height of a typical box (more space)
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 6, // Number of shimmer items
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 8.0),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.grey[300],
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 25,
+                                color: Colors.grey[300],
+                              ),
+                              const SizedBox(height: 15),
+                              Container(
+                                width: 100,
+                                height: 16,
+                                color: Colors.grey[300],
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Container(
+                          width: 50, // Adjust width according to your design
+                          height: 50, // Adjust height according to your design
+                          color: Colors.grey[300],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+
+
+
+      case 2:
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 0),
+          color: Colors.grey[200], // Grey background
+          child: Column(
+            children: [
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  height: 56.0,
+                  width: double.infinity,
+                  color: Colors.grey[300],
+                  child: Center(child: Text('Loading App Bar...')),
+                ),
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 4), // Apply padding
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: const Color.fromARGB(197, 208, 208, 208),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical:
+                                  30), // Apply vertical padding to the row
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                child: Container(
+                                  width: 90.0,
+                                  height: 36.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                ),
+                              ),
+                              Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                child: Container(
+                                  width: 90.0,
+                                  height: 36.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                ),
+                              ),
+                              Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                child: Container(
+                                  width: 90.0,
+                                  height: 36.0,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // const SizedBox(height: 8),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: 6,
+                            itemBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0,
+                                  horizontal: 16), // Apply vertical padding
+                              child: Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                child: Container(
+                                  height: 100.0,
+                                  width: double.infinity, // Ensure full width
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+
+      case 3:
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+            color: Colors.grey[300],
+            child: Center(child: Text('Loading Add Menu Page...')),
+          ),
+        );
+      case 4:
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 0),
+          color: Colors.grey[200], // Grey background
+          child: Column(
+            children: [
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  height: 56.0,
+                  width: double.infinity,
+                  color: Colors.grey[300],
+                  child: Center(child: Text('Loading App Bar...')),
+                ),
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 50.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 4), // Apply padding
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: const Color.fromARGB(197, 208, 208, 208),
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: 6,
+                            itemBuilder: (context, index) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0,
+                                  horizontal: 16), // Apply vertical padding
+                              child: Shimmer.fromColors(
+                                baseColor: Colors.grey[300]!,
+                                highlightColor: Colors.grey[100]!,
+                                child: Container(
+                                  height: 100.0,
+                                  width: double.infinity, // Ensure full width
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+
+      default:
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(
+            color: Colors.grey[300],
+          ),
+        );
+    }
   }
 }
--- zerowastapplication/lib/Scenes/splashscreen.dart
+++ zerowastapplication/lib/Scenes/splashscreen.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:zerowastapplication/Scenes/Mainmenu.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-  get splash => null;
-
   @override
-  Widget build(BuildContext context){
-    return AnimatedSplashScreen(splash: 
-    Column(
-      children: [
-        Center(
-          child: LottieBuilder.asset("assets/lottie/startloading.json"),
-        )
-      ],
-    ), nextScreen: const MenuScreen(),
-    splashIconSize: 400,
-    backgroundColor: Color(0xFFC7E8D5),);
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: LottieBuilder.asset("assets/lottie/loading.json"),
+          ),
+        ],
+      ),
+      nextScreen: const MenuScreen(),
+      splashIconSize: 400,
+      backgroundColor: Color(0xFFC7E8D5),
+      duration: 4700, // Set this to the duration of your Lottie animation in milliseconds
+      splashTransition: SplashTransition.fadeTransition, // Optional: choose your preferred transition
+      animationDuration: Duration(seconds: 1), // Optional: animation duration for the transition
+    );
   }
 }
